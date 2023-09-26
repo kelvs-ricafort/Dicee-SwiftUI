f497f074661a1cc14d1b8ef5262231029e4a6aac
Kelvin Ricafort <kelvs.r@outlook.com>
2023-09-26 14:37:22 +0800

    Completed

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -17,21 +21,24 @@ struct ContentView: View {
                 Image("diceeLogo")
                 Spacer()
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 1)
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
                 }
                 .padding(.horizontal)
+                
                 Spacer()
                 Button(action: {
-                    
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
                 }) {
                     Text("Roll")
                         .font(.system(size: 50))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
-                        .padding()
+                        .padding(.horizontal)
                 }
                 .background(Color.red)
+                .padding()
             }
         }
     }
