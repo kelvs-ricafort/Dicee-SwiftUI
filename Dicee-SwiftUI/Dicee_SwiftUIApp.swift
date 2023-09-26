d80a25cfc1ee0a7aaae88cb0751950ef1ef56575
Kelvin Ricafort <kelvs.r@outlook.com>
2023-09-26 13:40:23 +0800

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Kelvin Ricafort on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
